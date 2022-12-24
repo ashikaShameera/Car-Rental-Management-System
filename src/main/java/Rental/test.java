8ae467790f4d2f2684d6f25f6c6bbf86daf0e60f
Ashika Shameera <97051407+ashikaShameera@users.noreply.github.com>
2022-12-24 14:37:51 +0530

    Add files via upload

---
+++ src/main/java/Rental/test.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Rental;
+
+import view.Login;
+
+/**
+ *
+ * @author ashika
+ */
+public class test {
+    public static void main(String[] args) {
+        Login login=new Login();
+        login.show();
+    }
+}
